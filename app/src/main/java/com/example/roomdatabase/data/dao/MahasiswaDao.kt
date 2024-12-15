13063beb8852f4971fc09d7ec257dbe2e8bdf858
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-12-15 23:21:29 +0700

    menambah fun update mahasiwa

--- app/src/main/java/com/example/roomdatabase/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/roomdatabase/data/dao/MahasiswaDao.kt
@@ -1,8 +1,10 @@
 package com.example.roomdatabase.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.example.roomdatabase.data.entity.Mahasiswa
 import java.util.concurrent.Flow
 
@@ -10,10 +12,17 @@ import java.util.concurrent.Flow
 interface MahasiswaDao {
     @Insert
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa() : Flow<List<Mahasiswa>>
+
+    @Query("SELECT * FROM mahasiswa WHERE nim = :nim")
+    fun getMahasiswa(nim: String) : Flow<Mahasiswa>
+
+    @Delete
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
+
+    @Update
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
 }
 
-@Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
-fun getAllMahasiswa () : Flow<List<Mahasiswa>>
 
-@Query("SELECT * FROM mahasiswa WHERE nim = :nim")
-fun getMahasiswa (nim: String) : Flow<Mahasiswa>
