192e3cafce15437c1d1cd67bd8145de1b76a203b
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-12-17 01:07:37 +0700

    membuat data class

--- app/src/main/java/com/example/roomdatabase/ui/theme/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/ui/theme/viewmodel/DetailMhsViewModel.kt
@@ -5,10 +5,22 @@ import androidx.lifecycle.ViewModel
 import com.example.roomdatabase.repository.LocalRepositoryMhs
 import com.example.roomdatabase.repository.RepositoryMhs
 import com.example.roomdatabase.ui.theme.navigation.AlamatNavigasi
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+
+data class DetailUiState(
+)
 
 class DetailMhsViewModel(
     savedStateHandle: SavedStateHandle,
     private val repositoryMhs: RepositoryMhs,
 ) : ViewModel() {
-    private val nim: String = checkNotNull(savedStateHandle[AlamatNavigasi.DestinasiDetail.NIM])
+    private val _nim: String = checkNotNull(savedStateHandle[AlamatNavigasi.DestinasiDetail.NIM])
+
+    val detailUiState: StateFlow<DetailUiState> = repositoryMhs.getMhs(_nim)
+        .filterNotNull()
+        .map {
+            DetailUiEvent = it.toDetailEvent
+        }
 }
