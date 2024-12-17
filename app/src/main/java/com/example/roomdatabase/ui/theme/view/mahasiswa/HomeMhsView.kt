66412073ce6dad41e06e6e87bfc9bcad021d3661
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-12-17 17:39:50 +0700

    lengkapi homemhsview

--- app/src/main/java/com/example/roomdatabase/ui/theme/view/mahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/roomdatabase/ui/theme/view/mahasiswa/HomeMhsView.kt
@@ -2,10 +2,18 @@ package com.example.roomdatabase.ui.theme.view.mahasiswa
 
 import android.graphics.drawable.Icon
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -20,6 +28,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -132,5 +141,58 @@ fun ListMahasiswa(
     modifier: Modifier = Modifier,
     onClick: (String) -> Unit = {}
 ){
-
+LazyColumn(
+    modifier = Modifier
+) {
+    items(
+        items = listMhs,
+        itemContent = { mhs ->
+            CardsMhs (
+                mhs = mhs,
+                onClick = {onClick(mhs.nim)}
+            )
+        }
+    )
+}
+}
+@Composable
+fun CardsMhs (
+    mhs: Mahasiswa,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit = {},
+){
+    Card(
+        onClick = onClick,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Column (
+            modifier = Modifier.padding(8.dp)
+        ){
+           Row(
+               modifier = Modifier.fillMaxWidth(),
+               verticalAlignment = Alignment.CenterVertically
+           ) {
+               Icon(imageVector = Icons.Filled.Person, contentDescription = "")
+               Spacer(modifier =Modifier.padding(4.dp))
+               Text(
+                   text = mhs.nim,
+                   fontWeight = FontWeight.Bold,
+                   fontSize = 16.sp
+               )
+           }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.Home, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = mhs.kelas,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+    }
 }
