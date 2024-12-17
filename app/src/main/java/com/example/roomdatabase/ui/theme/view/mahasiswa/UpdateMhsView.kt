70f20ff0b1eaa1ae35fcb751b1dc1ca19c8c5dcf
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-12-17 17:44:32 +0700

    membuat fungsi UpdateMhsView

--- app/src/main/java/com/example/roomdatabase/ui/theme/view/mahasiswa/UpdateMhsView.kt
+++ app/src/main/java/com/example/roomdatabase/ui/theme/view/mahasiswa/UpdateMhsView.kt
@@ -1,2 +1,17 @@
 package com.example.roomdatabase.ui.theme.view.mahasiswa
 
+import PenyediaViewModel
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.roomdatabase.ui.theme.viewmodel.UpdateMhsViewModel
+
+@Composable
+fun UpdateMhsView(
+    onBack: () -> Unit,
+    OnNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: UpdateMhsViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
+}
